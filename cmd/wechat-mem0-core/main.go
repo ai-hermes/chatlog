25537953ec96316748af5b772b60cb9e939ec558
warjiang <1096409085@qq.com>
2026-01-09 14:44:36 +0800

    refactor(manager): split manager into CLI and GRPC implementations
    Extract manager interface and create separate implementations for CLI and GRPC usage. Update all call sites to use the new factory method with appropriate manager type. This improves code organization and prepares for better separation of concerns between different usage modes.

--- cmd/wechat-mem0-core/main.go
+++ cmd/wechat-mem0-core/main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"fmt"
 
-	"github.com/rs/zerolog/log"
 	"github.com/sjzar/chatlog/internal/chatlog"
 )
 
 func main() {
-	m := chatlog.New()
-	ret, err := m.CommandKeyForGRPC("", 0, false, false)
-	if err != nil {
-		log.Err(err).Msg("failed to get key")
-		return
-	}
-	fmt.Println(ret)
+	m := chatlog.New(chatlog.ManagerTypeGRPC)
+	fmt.Println(m)
+	//ret, err := m.CommandKeyForGRPC("", 0, false, false)
+	//if err != nil {
+	//	log.Err(err).Msg("failed to get key")
+	//	return
+	//}
+	//fmt.Println(ret)
 }
