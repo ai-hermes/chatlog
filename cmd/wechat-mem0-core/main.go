b828bce2f12b91635e697de3056d95c713c380b3
warjiang <1096409085@qq.com>
2026-01-09 15:06:47 +0800

    feat: add key retrieval and error handling to main
    Add GetKey functionality with proper error handling for .github/workflows/crossbuild.ymlSIP enabled case

--- cmd/wechat-mem0-core/main.go
+++ cmd/wechat-mem0-core/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/rs/zerolog/log"
+	"github.com/sjzar/chatlog/internal/errors"
 
 	"github.com/sjzar/chatlog/internal/chatlog"
 )
@@ -9,6 +11,16 @@ import (
 func main() {
 	m := chatlog.New(chatlog.ManagerTypeGRPC)
 	fmt.Println(m)
+	keyData, err := m.GetKey("", 0, false, false)
+	if err != nil {
+		if errors.Is(err, errors.ErrSIPEnabled) {
+			log.Info().Msg("SIP is enabled")
+			return
+		}
+		log.Err(err).Msgf("failed to get key")
+		return
+	}
+	fmt.Println(keyData)
 	//ret, err := m.CommandKeyForGRPC("", 0, false, false)
 	//if err != nil {
 	//	log.Err(err).Msg("failed to get key")
