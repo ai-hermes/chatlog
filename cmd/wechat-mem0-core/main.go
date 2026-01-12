44a135d5d351efb0ad4c8ff9f4f88e6cf196cc61
warjiang <1096409085@qq.com>
2026-01-12 09:33:09 +0800

    feat(logging): add logging initialization with debug support and file output
    Initialize zerolog with configurable debug level and file output
    Add log file creation in specified misc directory
    Configure console and file output with consistent formatting

--- cmd/wechat-mem0-core/main.go
+++ cmd/wechat-mem0-core/main.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
+	"time"
+
 	_ "github.com/joho/godotenv/autoload"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/sjzar/chatlog/internal/chatlog"
-	"github.com/sjzar/chatlog/internal/wechatdb"
-	"os"
-	"strconv"
 )
 
+func initLog(debug bool, miscDir string) {
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	if debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
+	logPath := filepath.Join(miscDir, "logs")
+	logFD, err := os.OpenFile(filepath.Join(logPath, "wechat-mem0-core.log"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, NoColor: true, TimeFormat: time.RFC3339}).
+		Output(zerolog.ConsoleWriter{Out: logFD, NoColor: true, TimeFormat: time.RFC3339})
+}
+
 func main() {
+	miscDir := os.Getenv("MISC_DIR")
+	initLog(false, miscDir)
+
 	// debug 场景没法突破内存限制，用单独的命令行可以解析出来
 	m := chatlog.New(chatlog.ManagerTypeGRPC)
 	m = m
 	/*
+		// fetch key for decrypt WeChat
 		keyData, err := m.GetKey("", 0, false, false)
 		if err != nil {
 			if errors.Is(err, errors.ErrSIPEnabled) {
@@ -26,6 +49,7 @@ func main() {
 		fmt.Println(keyData)
 	*/
 	/*
+		// WeChat management
 		instances := m.GetWeChatInstances()
 		if len(instances) > 0 {
 			instance := instances[0]
@@ -38,6 +62,7 @@ func main() {
 		}
 	*/
 	/*
+		// decrypt WeChat data manually
 		cmdConf := make(map[string]any)
 		cmdConf["platform"] = os.Getenv("PLATFORM")
 		cmdConf["version"] = os.Getenv("VERSION")
@@ -53,39 +78,120 @@ func main() {
 		log.Info().Msg("decrypt success")
 	*/
 
-	// db exporter
-	//ddl, err := pgmigrate.GenerateDDL(os.Getenv("DB_PATH"))
-	//if err != nil {
-	//	log.Err(err).Msg("failed to generate ddl")
-	//	return
-	//}
-	//log.Info().Str("ddl", ddl).Msg("generated ddl")
+	/*
+			// start http server
+		cmdConf := make(map[string]any)
+		cmdConf["platform"] = os.Getenv("PLATFORM")
+		cmdConf["version"] = os.Getenv("VERSION")
+		cmdConf["img_key"] = os.Getenv("IMG_KEY")
+		cmdConf["data_key"] = os.Getenv("DATA_KEY")
+		cmdConf["data_dir"] = os.Getenv("DATA_DIR")
+		cmdConf["work_dir"] = os.Getenv("WORK_DIR")
+		cmdConf["http_addr"] = os.Getenv("HTTP_ADDR")
 
-	workDir := os.Getenv("WORK_DIR")
-	platform := os.Getenv("PLATFORM")
-	version, _ := strconv.Atoi(os.Getenv("VERSION"))
-	db, err := wechatdb.New(workDir, platform, version)
-	if err != nil {
-		log.Fatal().Err(err).Msg("failed to create db")
-		return
-	}
-	contacts, err := db.GetContacts("", 0, 0)
-	if err != nil {
-		log.Fatal().Err(err).Msg("failed to get contacts")
-		return
-	}
-	//log.Info().Interface("contacts", contacts).Msg("get contacts")
-	for _, contact := range contacts.Items {
-		log.Info().Interface("contact", contact).Msg("contact")
-	}
+		log.Info().Msgf("server cmd config: %+v", cmdConf)
 
-	//dbFiles, err := pgmigrate.ListDBFiles(os.Getenv("DB_PATH"))
-	//if err != nil {
-	//	log.Err(err).Msg("failed to list db files")
-	//	return
-	//}
-	//
-	//for _, dbFile := range dbFiles {
-	//	log.Info().Str("file", dbFile).Msg("loading db file")
-	//}
+		if err := m.CommandHTTPServer("", cmdConf); err != nil {
+			log.Err(err).Msg("failed to start server")
+			return
+		}
+	*/
+	/*
+		// db usage example
+		workDir := os.Getenv("WORK_DIR")
+		platform := os.Getenv("PLATFORM")
+		version, _ := strconv.Atoi(os.Getenv("VERSION"))
+		db, err := wechatdb.New(workDir, platform, version)
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to create db")
+			return
+		}
+		db = db
+
+		// 1. list contacts method
+		contacts, err := db.GetContacts("", 0, 0)
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to get contacts")
+			return
+		}
+		for _, contact := range contacts.Items {
+			log.Info().Interface("contact", contact).Msg("contact")
+		}
+
+		// 2. chat rooms method
+		rooms, err := db.GetChatRooms("", 0, 0)
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to get rooms")
+			return
+		}
+		for _, room := range rooms.Items {
+			log.Info().Interface("room", room).Msg("room")
+		}
+
+		// 3. chat session
+		sessions, err := db.GetSessions("", 0, 0)
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to get sessions")
+			return
+		}
+		for _, session := range sessions.Items {
+			log.Info().Interface("session", session).Msg("session")
+		}
+
+		// 4. chat messages
+		messages, err := db.GetMessages(time.Unix(0, 0), time.Now(), "何欢", "", "", 0, 0)
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to get messages")
+			return
+		}
+		for _, message := range messages {
+			log.Info().Interface("message", message.PlainTextContent()).Msg("message")
+		}
+	*/
+
+	/*
+		// WeChat Media file
+		mediaPath := os.Getenv("IMAGE_MEDIA_FILE_PATH
+		b, err := os.ReadFile(mediaPath)
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to read file")
+			return
+		}
+		out, ext, err := dat2img.Dat2Image(b)
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to read file")
+			return
+		}
+		log.Info().Str("ext", ext).Msg("ext")
+
+		outputPath := os.Getenv("IMAGE_MEDIA_FILE_OUTPUT_PATH") + "wechat_image." + ext
+		if err := os.WriteFile(outputPath, out, 0644); err != nil {
+			log.Fatal().Err(err).Msg("failed to write file")
+			return
+		}
+		log.Info().Str("path", outputPath).Msg("image saved")
+	*/
+
+	/*
+		// db migration
+		dbFiles, err := pgmigrate.ListDBFiles(os.Getenv("DB_PATH"))
+		if err != nil {
+			log.Err(err).Msg("failed to list db files")
+			return
+		}
+
+		for _, dbFile := range dbFiles {
+			log.Info().Str("file", dbFile).Msg("loading db file")
+		}
+	*/
+
+	/*
+		// db exporter
+		ddl, err := pgmigrate.GenerateDDL(os.Getenv("DB_PATH"))
+		if err != nil {
+			log.Err(err).Msg("failed to generate ddl")
+			return
+		}
+		log.Info().Str("ddl", ddl).Msg("generated ddl")
+	*/
 }
